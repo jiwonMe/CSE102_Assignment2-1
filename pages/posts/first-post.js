e3a8a41bd269a6a18508ec22f4df3384f065d238
Jiwon Park <park@jiwon.me>
2021-04-12 21:56:12 +0900

    ADD nothing

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function FirstPost() {
   return (
     <>
-      <h1>My names is Jiwon Park</h1>
+      <h1>My names is Jiwon Park.</h1>
 	  <p>My hobby: coding</p>
       <h2>
         <Link href="/">
